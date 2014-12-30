2270511cba6514a4391e10fb669b014e03290baf
elkabumi <elkabuminew@gmail.com>
2014-12-30 19:38:20 +0700

    20-12-2014

---
+++ tabel_tambahan_answer_kab_kota.sql
@@ -0,0 +1,103 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `a_1_1_3`
+--
+
+CREATE TABLE IF NOT EXISTS `a_1_1_3` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer_id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `a_answer_point` int(11) NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `a_1_1_3`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `a_1_2_2`
+--
+
+CREATE TABLE IF NOT EXISTS `a_1_2_2` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer_id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `a_answer_number` int(11) NOT NULL,
+  `a_answer_type` int(11) NOT NULL,
+  `a_answer` varchar(11) NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `a_1_2_2`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `a_1_3_2`
+--
+
+CREATE TABLE IF NOT EXISTS `a_1_3_2` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer_id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `a_answer_number` int(11) NOT NULL,
+  `a_answer_year` int(11) NOT NULL,
+  `a_answer` int(11) NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `a_1_3_2`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `a_1_3_3`
+--
+
+CREATE TABLE IF NOT EXISTS `a_1_3_3` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer_id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `a_answer_number` int(11) NOT NULL,
+  `a_answer_type` int(11) NOT NULL,
+  `a_answer` varchar(11) NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `a_1_3_3`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `a_1_4_1`
+--
+
+CREATE TABLE IF NOT EXISTS `a_1_4_1` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer_id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `a_answer` int(11) NOT NULL,
+  `a_answer_description` text NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `a_1_4_1`
+--
+
+
+-- --------------------------------------------------------
